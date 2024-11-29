f3295c5f8dfeedd24fd33147fc5da057320412bd
Kelavaa <lukakelava@ssklivno.net>
2024-11-29 09:35:34 +0100

    izmjena
    dodo igru treba jos petlju da ponavlja i sve malo sredit

--- projekt.py
+++ projekt.py
@@ -1,5 +1,32 @@
 import random
-izbori=["paper","rock","scisors"]
-izbor=random.choice(izbori)
-print(izbor)
+
+scoreBota = 0
+scoreKorisnika = 0
+izborBota = random.randint(1,3)
+#1 = kamen
+#2 = papir
+#3 = skare
+
+print("Unesite 1 za kamen, 2 za papir i 3 za makaze")
+izborKorisnika = int(input())
+if(izborBota==izborKorisnika):
+    print("Nitko nije pobjedio")
+if((izborBota==1) and (izborKorisnika==2)):
+    print("Izgubio si")
+    scoreBota +=1
+if(izborBota==1 and izborKorisnika==3):
+    print("Pobjedio si")
+    scoreKorisnika += 1
+if(izborBota==2 and izborKorisnika==3):
+    print("Pobjedio si")
+    scoreKorisnika += 1
+if(izborBota==2 and izborKorisnika==1):
+    print("Izgubio si")
+    scoreBota+=0
+if(izborBota==3 and izborKorisnika==1):
+    print("Pobjedio si")
+    scoreKorisnika+=1
+if(izborBota==3 and izborKorisnika==2):
+    print("Izgubio si")
+    scoreBota +=1
 
