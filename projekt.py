8b6ae584aa6ea1a0696db176fb8ecdf88f7617fd
Kelavaa <lukakelava@ssklivno.net>
2024-12-05 10:02:25 +0100

    update
    dodo loop

--- projekt.py
+++ projekt.py
@@ -2,31 +2,36 @@ import random
 
 scoreBota = 0
 scoreKorisnika = 0
-izborBota = random.randint(1,3)
-#1 = kamen
-#2 = papir
-#3 = skare
+while True:
+    izborBota = random.randint(1,3)
+    #1 = kamen
+    #2 = papir
+    #3 = skare
 
-print("Unesite 1 za kamen, 2 za papir i 3 za makaze")
-izborKorisnika = int(input())
-if(izborBota==izborKorisnika):
-    print("Nitko nije pobjedio")
-if((izborBota==1) and (izborKorisnika==2)):
-    print("Izgubio si")
-    scoreBota +=1
-if(izborBota==1 and izborKorisnika==3):
-    print("Pobjedio si")
-    scoreKorisnika += 1
-if(izborBota==2 and izborKorisnika==3):
-    print("Pobjedio si")
-    scoreKorisnika += 1
-if(izborBota==2 and izborKorisnika==1):
-    print("Izgubio si")
-    scoreBota+=0
-if(izborBota==3 and izborKorisnika==1):
-    print("Pobjedio si")
-    scoreKorisnika+=1
-if(izborBota==3 and izborKorisnika==2):
-    print("Izgubio si")
-    scoreBota +=1
+    print("Unesite 1 za kamen, 2 za papir i 3 za makaze")
+    izborKorisnika = int(input())
+    if(izborBota==izborKorisnika):
+        print("Nitko nije pobjedio")
+    if((izborBota==1) and (izborKorisnika==2)):
+        print("Izgubio si")
+        scoreBota +=1
+    if(izborBota==1 and izborKorisnik1a==3):
+        print("Pobjedio si")
+        scoreKorisnika += 1
+    if(izborBota==2 and izborKorisnika==3):
+        print("Pobjedio si")
+        scoreKorisnika += 1
+    if(izborBota==2 and izborKorisnika==1):
+        print("Izgubio si")
+        scoreBota+=1
+    if(izborBota==3 and izborKorisnika==1):
+        print("Pobjedio si")
+        scoreKorisnika+=1
+    if(izborBota==3 and izborKorisnika==2):
+        print("Izgubio si")
+        scoreBota +=1
+    if(izborKorisnika==0):
+        print("vas rezultat je: ", scoreKorisnika)
+        print("botov rezultat je: ", scoreBota)
+        break
 
